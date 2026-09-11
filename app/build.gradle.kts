ec2452253ebe8eedffacf4abee875cf8f48b2623
DESKTOP-H9VG51M\Adi <jakarta>
2026-09-11 17:04:48 +0700

    refactor: replace project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,15 +6,15 @@ plugins {
 android {
     namespace = "id.adiyusuf.compressapp"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
+        version = release(37) {
+            minorApiLevel = 0
         }
     }
 
     defaultConfig {
         applicationId = "id.adiyusuf.compressapp"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
